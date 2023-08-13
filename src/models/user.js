d10178a13587a07af0e3960d0451384001c27aa3
Nabaz esmail <nabazesmail567@gmail.com>
2023-08-13 19:00:45 +0300

    validation for email and password done!

--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,54 @@
 // src/models/user.js
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  // No need to include ID field here
-  email: String,
-  password: String,
+  username: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (value) {
+        return /\S+@\S+\.\S+/.test(value);
+      },
+      message: 'Email address must be valid',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 15,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+userSchema.pre('findOneAndUpdate', async function (next) {
+  if (!this._update.password) {
+    return next();
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(this._update.password, 10);
+    this._update.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
-
-
